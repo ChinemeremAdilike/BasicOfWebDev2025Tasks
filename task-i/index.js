e7fe2f55f07dc978151c4adfb5bc558afa5d5a4d
Chinemerem Adilike <chinemerem.adilike@centria.fi>
2025-11-20 07:41:27 +0200

    Add Task-i (html + js)

---
+++ task-i/index.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    alert("Redirecting to rental listings...");
+    // You can replace this with actual navigation logic
+  });
+});
