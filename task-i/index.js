d29d68a0e1acd9244a593ca90d505d94a2d656f4
Chinemerem Adilike <chinemerem.adilike@centria.fi>
2025-11-27 19:03:41 +0200

    Add catalog html + mp4 + jpg

--- task-i/index.js
+++ task-i/index.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector("button");
   button.addEventListener("click", () => {
     alert("Redirecting to rental listings...");
-    // You can replace this with actual navigation logic
+    
   });
 });
